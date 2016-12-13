3ab842f7f639d143f8262d237a629f62ed007b34
unknown <lucas_1@VENUS.weblakes.com>
2016-12-13 16:14:08 -0500

    basic implementation of gridview, needs to implement custom TextView

--- Keyboard/MainActivity.cs
+++ Keyboard/MainActivity.cs
@@ -13,6 +13,15 @@ namespace Keyboard
 	{
 		int count = 1;
 
+		//Emoji[] data = { new Emoji("aaa"), new Emoji("bbb"), new Emoji("ccc"), new Emoji("ddd"), new Emoji("eee"), new Emoji("fff"),
+		//	new Emoji("ggg"), new Emoji("hhh"), new Emoji("iii"), new Emoji("jjj"), new Emoji("lll"), new Emoji("mmm"), new Emoji("nnn"),
+		//	new Emoji("ooo"), new Emoji("ppp"), new Emoji("qqq"), new Emoji("rrr"), new Emoji("sss"), new Emoji("ttt"), new Emoji("uuu"),
+		//	new Emoji("vvv"), new Emoji("xxx"), new Emoji("zzz")};
+
+		Emoji[] data = PopulateData(-1);
+
+		//string[] data = { "aaa", "bbb", "ccc" };
+
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
 			base.OnCreate(savedInstanceState);
@@ -25,7 +34,7 @@ namespace Keyboard
 			Button emoji_button = FindViewById<Button>(Resource.Id.emoji_button);
 			emoji_button.Click += ShowEmojisTable;
 
-			emoji_button = FindViewById<Button>(Resource.Id.teste);
+			/*emoji_button = FindViewById<Button>(Resource.Id.teste);
 			emoji_button.Click += delegate {
 				var txt = FindViewById<EditText>(Resource.Id.input_text);
 				string k = txt.Text;
@@ -38,18 +47,53 @@ namespace Keyboard
 				//txt.SetText(ss);
 				//txt.SetSelection(txt.Text.Length);
 
-			};
+			};*/
 		}
 
 		public void ShowEmojisTable(object sender, System.EventArgs e)
 		{
 			var txt = FindViewById<EditText>(Resource.Id.input_text);
-			txt.Text = "test";
+			//txt.Text = "test";
 			txt.SetSelection(txt.Text.Length);
 			InputMethodManager imm = (InputMethodManager)GetSystemService(Context.InputMethodService);
 			imm.HideSoftInputFromWindow(txt.WindowToken, 0);
 
+
+
+			var grid = FindViewById<GridView>(Resource.Id.emoji_gridview);
+			grid.Adapter = new EmojiAdapter(this, data);
+			//grid.Adapter = new ArrayAdapter(this, Resource.Layout.TextViewItem, data);
+			grid.ItemClick += delegate (object Sender, AdapterView.ItemClickEventArgs args)
+			{
+				txt.Text = "test";
+			};
+
+			grid.Visibility = Android.Views.ViewStates.Visible;
+
 		}
+
+		//Temporary mechanism for populating data with emojis
+		public static Emoji[] PopulateData(int size)
+		{
+			string[] codes = { ":grinning:", ":grin:", ":joy:", ":rofl:", ":smiley:", ":smile:", ":sweat_smile:", ":laughing:",
+				":wink:", ":blush:", ":yum:", ":sunglasses:", ":heart_eyes:", ":kissing_heart:", ":kissing:", ":kissing_smiling_eyes:",
+				":kissing_closed_eyes:", ":relaxed:", ":slight_smile:", ":hugging:", ":thinking:", ":neutral_face:", ":expressionless:",
+				":no_mouth:", ":rolling_eyes:", ":smirk:", ":persevere:", ":disappointed_relieved:", ":open_mouth:", ":zipper_mouth:",
+				":hushed:", ":sleepy:", ":tired_face:", ":sleeping:", ":relieved:", ":nerd:", ":stuck_out_tongue:",
+				":stuck_out_tongue_winking_eye:", ":stuck_out_tongue_closed_eyes:", ":drooling_face:" };
+
+			if (size <= 0 || size >= codes.Length)
+				size = codes.Length;
+
+			Emoji[] emojiRay = new Emoji[size];
+
+			for (int i = 0; i < size;i++){
+				emojiRay[i] = new Emoji(codes[i]);
+			}
+			return emojiRay;
+		}
+			
+		
 	}
 }
 
---
+++ Keyboard/OnEmojiClickedListener.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace Keyboard
+{
+	public interface OnEmojiClickedListener
+	{
+		void onEmojiClicked(Emoji emoji);
+	}
+}
